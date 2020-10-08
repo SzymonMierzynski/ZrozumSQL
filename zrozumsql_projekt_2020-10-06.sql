ef99f89408bcdd6bcfd4083960b85b29c418abba
Szymon Mierzynski <szymon.mierzynski@gmail.com>
2020-10-08 11:51:16 +0200

    2020.10.08

--- zrozumsql_projekt_2020-10-06.sql
+++ zrozumsql_projekt_2020-10-06.sql
@@ -1,4 +1,4 @@
---zrozumsql projekt cz1 2020.10.07
+--zrozumsql projekt cz1 2020.10.08
 
 --1. Utwórz nowy schemat a jeœli istnieje do usuñ
 --DROP SCHEMA IF EXISTS expense_tracker CASCADE; 
@@ -8,44 +8,60 @@ CREATE SCHEMA IF NOT EXISTS expense_tracker;
 ---------------------------------
 
 --utwórz tabelê bank_account_owner a jeœli istnienie usuñ
+-----CREATE TABLE IF NOT EXISTS EXPENSE_TRACKER.bank_account_owner
 
---DROP TABLE IF EXISTS expense_tracker.bank_account_owner CASCADE;
+--Tabela: bank_account_owner
+--Kolumny:
+-- id_ba_own, typ ca³kowity, klucz g³ówny
+-- owner_name, typ tekstowy 50 znaków, not null
+-- owner_desc, typ tekstowy 250 znaków
+-- user_login, typ ca³kowity, not null
+-- active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, not null
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
-CREATE TABLE IF NOT EXISTS EXPENSE_TRACKER.bank_account_owner
+DROP TABLE IF EXISTS expense_tracker.bank_account_owner CASCADE;
+
+CREATE TABLE EXPENSE_TRACKER.bank_account_owner
 (
-	id_ba_own int PRIMARY key, 							--typ ca³kowity, klucz g³ówny
-	owner_name varchar(50) NOT NULL, 					--typ tekstowy 50 znaków, not null
-	owner_desc varchar(250), 							--typ tekstowy 250 znaków
-	user_login int NOT NULL, 							--typ ca³kowity, not null
-	active boolean DEFAULT TRUE NOT null, 				--typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego
-														--wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, not null
-	
-	insert_date timestamp DEFAULT current_timestamp, 	--typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje
-														--wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
-	
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	id_ba_own int PRIMARY KEY,
+	owner_name varchar(50) NOT NULL,
+	owner_desc varchar(250),
+	user_login int NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL,
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
 
 --INSERT INTO EXPENSE_TRACKER.BANK_ACCOUNT_OWNER VALUES (5, 'OWNER','own_descr', 123);
 
 --SELECT * FROM EXPENSE_TRACKER.BANK_ACCOUNT_OWNER; 
 
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
 
---utwórz tabelê bank_account_owner a jeœli istnienie usuñ
+--Tabela: bank_account_types
+--Kolumny:
+-- id_ba_type, typ ca³kowity, klucz g³ówny
+-- ba_type, typ tekstowy 50 znaków, not null
+-- ba_desc, typ tekstowy 250 znaków
+-- active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, , not null
+-- is_common_account, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 0, dla prawdy fa³sz ustaw fa³sz, , not null
+-- id_ba_own, typ ca³kowity
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
---DROP TABLE IF EXISTS expense_tracker.bank_account_types CASCADE;
+DROP TABLE IF EXISTS expense_tracker.bank_account_types CASCADE;
 
-CREATE TABLE IF NOT EXISTS expense_tracker.bank_account_types 
+CREATE TABLE expense_tracker.bank_account_types 
 (
-	id_ba_type int PRIMARY key, 						--typ ca³kowity, klucz g³ówny
-	ba_type varchar(50) NOT null, 						--typ tekstowy 50 znaków, not null
-	ba_desc varchar(250), 								--typ tekstowy 250 znaków
-	active boolean DEFAULT true, 						--active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean))
-	is_common_account boolean NOT NULL DEFAULT false, 	--typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu
-	id_ba_own int, 										--typ ca³kowity
-	insert_date timestamp DEFAULT current_timestamp,	--typ data i czas, wartoœæ domyœlna current_timestamp
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	id_ba_type int PRIMARY KEY,
+	ba_type varchar(50) NOT NULL,
+	ba_desc varchar(250),
+	active boolean DEFAULT TRUE,
+	is_common_account boolean NOT NULL DEFAULT FALSE,
+	id_ba_own int,
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
 
 --INSERT INTO EXPENSE_TRACKER.BANK_ACCOUNT_TYPES VALUES (1, 'typ1','descr1');
@@ -54,106 +70,158 @@ CREATE TABLE IF NOT EXISTS expense_tracker.bank_account_types
 
 ----------------------------------------------------------------------------------
 
---utwórz tabelê transactions a jeœli istnienie usuñ
+--Tabela: transactions
+--Kolumny:
+-- id_transaction, typ ca³kowity, klucz g³ówny
+-- id_trans_ba, typ ca³kowity
+-- id_trans_cat, typ ca³kowity
+-- id_trans_subcat, typ ca³kowity
+-- id_trans_type, typ ca³kowity
+-- id_user, typ ca³kowity
+-- transaction_date, typ daty (sama data), wartoœæ domyœlna current_date (spowoduje wstawianie aktualnej daty w momencie wstawiania wierszy)
+-- transaction_value, typ zmiennoprzecinkowy (numeric, 9 znaków, do 2 znaków po przecinku)
+-- transaction_description, typ tekstowy (nieograniczony)
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
---DROP TABLE IF EXISTS expense_tracker.transactions CASCADE;
+DROP TABLE IF EXISTS expense_tracker.transactions CASCADE;
 
-CREATE TABLE IF NOT EXISTS expense_tracker.transactions
+CREATE TABLE expense_tracker.transactions
 (
-	id_transaction int PRIMARY key, 					--typ ca³kowity, klucz g³ówny
-	id_trans_ba int, 									--typ ca³kowity
-	id_trans_cat int, 									--typ ca³kowity
-	id_trans_subcat int, 								--typ ca³kowity
-	id_trans_type int, 									--typ ca³kowity
-	id_user int, 										--typ ca³kowity
+	id_transaction int PRIMARY KEY,
+	id_trans_ba int,
+	id_trans_cat int,
+	id_trans_subcat int,
+	id_trans_type int,
+	id_user int,
 	transaction_date date DEFAULT current_date,
 	transaction_value NUMERIC(9,2),
 	transaction_description TEXT,
-	insert_date timestamp DEFAULT current_timestamp,	--typ data i czas, wartoœæ domyœlna current_timestamp
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
 
 ----------------------------------------------------------------------------------
 
---utwórz tabelê transaction_bank_accounts a jeœli istnienie usuñ
+--Tabela: transaction_bank_accounts
+--Kolumny:
+-- id_trans_ba, typ ca³kowity, klucz g³ówny
+-- id_ba_own, typ ca³kowity,
+-- id_ba_typ, typ ca³kowity,
+-- bank_account_name, typ tekstowy 50 znaków, not null
+-- bank_account_desc, typ tekstowy 250 znaków
+-- active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, , not null
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
---DROP TABLE IF EXISTS expense_tracker.transaction_bank_accounts CASCADE;
+DROP TABLE IF EXISTS expense_tracker.transaction_bank_accounts CASCADE;
 
-CREATE TABLE IF NOT EXISTS expense_tracker.transaction_bank_accounts
+CREATE TABLE expense_tracker.transaction_bank_accounts
 (
-	id_trans_ba int PRIMARY key, 						--typ ca³kowity, klucz g³ówny
-	id_ba_own int, 										--typ ca³kowity,
-	id_ba_typ int, 										--typ ca³kowity,
-	bank_account_name varchar(50) NOT NULL, 			--typ tekstowy 50 znaków, not null
-	bank_account_desc varchar(250), 					--typ tekstowy 250 znaków
-	active boolean NOT NULL DEFAULT TRUE, 				--active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean))
-	insert_date timestamp DEFAULT current_timestamp,	--typ data i czas, wartoœæ domyœlna current_timestamp
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	id_trans_ba int PRIMARY KEY,
+	id_ba_own int,
+	id_ba_typ int,
+	bank_account_name varchar(50) NOT NULL,
+	bank_account_desc varchar(250),
+	active boolean NOT NULL DEFAULT TRUE,
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
 
 ----------------------------------------------------------------------------------
 
---utwórz tabelê transaction_category a jeœli istnienie usuñ
+--Tabela: transaction_category
+--Kolumny:
+-- id_trans_cat, typ ca³kowity, klucz g³ówny
+-- category_name, typ tekstowy 50 znaków, not null
+-- category_description, typ tekstowy 250 znaków
+-- active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, , not null
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
---DROP TABLE IF EXISTS expense_tracker.transaction_category CASCADE;
+DROP TABLE IF EXISTS expense_tracker.transaction_category CASCADE;
 
-CREATE TABLE IF NOT EXISTS expense_tracker.transaction_category
+CREATE TABLE expense_tracker.transaction_category
 (
-	id_trans_cat int PRIMARY KEY, 						--typ ca³kowity, klucz g³ówny
-	category_name varchar(50) NOT NULL, 				--typ tekstowy 50 znaków, not null
-	category_description varchar(250), 					--typ tekstowy 250 znaków
-	active boolean NOT NULL DEFAULT TRUE, 				--active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean))
-	insert_date timestamp DEFAULT current_timestamp,	--typ data i czas, wartoœæ domyœlna current_timestamp
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	id_trans_cat int PRIMARY KEY,
+	category_name varchar(50) NOT NULL,
+	category_description varchar(250),
+	active boolean NOT NULL DEFAULT TRUE,
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
 
 ----------------------------------------------------------------------------------
 
---utwórz tabelê transaction_subcategory a jeœli istnienie usuñ
+--Tabela: transaction_subcategory
+--Kolumny:
+-- id_trans_subcat, typ ca³kowity, klucz g³ówny
+-- id_trans_cat, typ ca³kowity
+-- subcategory_name, typ tekstowy 50 znaków, not null
+-- subcategory_description, typ tekstowy 250 znaków
+-- active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, , not null
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
---DROP TABLE IF EXISTS expense_tracker.transaction_subcategory CASCADE;
+DROP TABLE IF EXISTS expense_tracker.transaction_subcategory CASCADE;
 
-CREATE TABLE IF NOT EXISTS expense_tracker.transaction_subcategory
+CREATE TABLE expense_tracker.transaction_subcategory
 (
-	id_trans_subcat int PRIMARY KEY,					--typ ca³kowity, klucz g³ówny
-	id_trans_cat int, 									--typ ca³kowity, klucz g³ówny
-	subcategory_name varchar(50) NOT NULL, 				--typ tekstowy 50 znaków, not null
-	subcategory_description varchar(250), 				--typ tekstowy 250 znaków
-	active boolean NOT NULL DEFAULT TRUE, 				--active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean))
-	insert_date timestamp DEFAULT current_timestamp,	--typ data i czas, wartoœæ domyœlna current_timestamp
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	id_trans_subcat int PRIMARY KEY,
+	id_trans_cat int,
+	subcategory_name varchar(50) NOT NULL,
+	subcategory_description varchar(250),
+	active boolean NOT NULL DEFAULT TRUE,
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
 
 ----------------------------------------------------------------------------------
 
---utwórz tabelê transaction_subcategory a jeœli istnienie usuñ
+--Tabela: transaction_type
+--Kolumny:
+-- id_trans_type, typ ca³kowity, klucz g³ówny
+-- transaction_type_name, typ tekstowy 50 znaków, not null
+-- transaction_type_desc, typ tekstowy 250 znaków
+-- active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, , not null
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
---DROP TABLE IF EXISTS expense_tracker.transaction_type CASCADE;
+DROP TABLE IF EXISTS expense_tracker.transaction_type CASCADE;
 
-CREATE TABLE IF NOT EXISTS expense_tracker.transaction_type
+CREATE TABLE expense_tracker.transaction_type
 (
-	id_trans_type int PRIMARY KEY, 						--typ ca³kowity, klucz g³ówny
-	transaction_type_name varchar(50) NOT NULL, 		--typ tekstowy 50 znaków, not null
-	transaction_type_desc varchar(250), 				--typ tekstowy 250 znaków
-	active boolean NOT NULL DEFAULT TRUE, 				--active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean))
-	insert_date timestamp DEFAULT current_timestamp,	--typ data i czas, wartoœæ domyœlna current_timestamp
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	id_trans_type int PRIMARY KEY,
+	transaction_type_name varchar(50) NOT NULL,
+	transaction_type_desc varchar(250),
+	active boolean NOT NULL DEFAULT TRUE,
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
 
 ----------------------------------------------------------------------------------
 
---utwórz tabelê users a jeœli istnienie usuñ
+--Tabela: users
+--Kolumny:
+-- id_user, typ ca³kowity, klucz g³ówny
+-- user_login, typ tekstowy 25 znaków, not null
+-- user_name, typ tekstowy 50 znaków, not null
+-- user_password, typ tekstowy 100 znaków, not null
+-- password_salt, typ tekstowy 100 znaków, not null
+-- active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean)), dla typu znakowego wartoœæ domyœlna 1, dla prawdy fa³sz ustaw prawdê, , not null
+-- insert_date, typ data i czas, wartoœæ domyœlna current_timestamp (spowoduje wstawianie aktualnej daty i czasu w momencie wstawiania wierszy)
+-- update_date, typ data i czas, wartoœæ domyœlna current_timestamp
 
---DROP TABLE IF EXISTS expense_tracker.users CASCADE;
+DROP TABLE IF EXISTS expense_tracker.users CASCADE;
 
-CREATE TABLE IF NOT EXISTS expense_tracker.users
+CREATE TABLE expense_tracker.users
 (
-	id_user int PRIMARY KEY, 							--typ ca³kowity, klucz g³ówny
-	user_login varchar(25) NOT NULL, 					--typ tekstowy 25 znaków, not null
-	user_name varchar(50) NOT NULL, 					--typ tekstowy 50 znaków, not null
-	user_password varchar(100) NOT NULL, 				--typ tekstowy 100 znaków, not null
-	password_salt varchar(100) NOT NULL, 				--typ tekstowy 100 znaków, not null
-	active boolean NOT NULL DEFAULT TRUE, 				--active, typ tekstowy 1 znak (lub prawda / fa³sz (boolean))
-	insert_date timestamp DEFAULT current_timestamp,	--typ data i czas, wartoœæ domyœlna current_timestamp
-	update_date timestamp DEFAULT current_timestamp 	--typ data i czas, wartoœæ domyœlna current_timestamp
+	id_user int PRIMARY KEY,
+	user_login varchar(25) NOT NULL,
+	user_name varchar(50) NOT NULL,
+	user_password varchar(100) NOT NULL,
+	password_salt varchar(100) NOT NULL,
+	active boolean NOT NULL DEFAULT TRUE,
+	insert_date timestamp DEFAULT current_timestamp,
+	update_date timestamp DEFAULT current_timestamp
 );
